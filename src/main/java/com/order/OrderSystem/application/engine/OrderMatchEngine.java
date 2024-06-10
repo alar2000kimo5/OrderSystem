ecfee3f5c04f5316ef09b3c8582fa945e3cd1acb
alan <alar2000kimo5@gmail.com>
2024-06-10 09:49:32 +0800

    1.重構嘗試讓matchEngine可以match其它物件 2.進入測試

--- src/main/java/com/order/OrderSystem/application/engine/OrderMatchEngine.java
+++ src/main/java/com/order/OrderSystem/application/engine/OrderMatchEngine.java
@@ -8,16 +8,35 @@ import com.order.OrderSystem.domain.OrderMatchEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Component
 public class OrderMatchEngine extends MatchEngine<Order, OrderUseCase> {
     // jpa db
     @Autowired
     OrderRepository orderRepository;
+    @Autowired
+    RedisQueueZSetService redisQueueZSetService;
 
     OrderMatchEngine(Class<OrderUseCase> useCaseClass) {
         super(useCaseClass);
     }
 
+    /**
+     * 父層metchEngine會依照給出的queue，多緒上鎖後呼叫Runnable方法
+     * 這裡給出上鎖後要做什麼事
+     * */
+    @Override
+    protected Runnable lockAndRun(String lockQueueName) {
+        return () -> matchAndSave(lockQueueName, getMatchData(lockQueueName));
+    }
+    /**
+     * 這裡寫入2個物件如何匹配
+     * obj1 主匹配
+     * obj2 被匹配
+     */
     @Override
     public boolean isMatch(Order obj1, Order obj2) {
         if (obj1.getInComeType() == obj2.getInComeType()) {
@@ -28,13 +47,45 @@ public class OrderMatchEngine extends MatchEngine<Order, OrderUseCase> {
                 && obj1.getPriceType().equals(obj2.getPriceType())
                 && obj1.getPrice().equals(obj2.getPrice());
     }
+    /**
+     * 這裡處理匹配後要做事，此例為：1.寫入db 2.刪除queue上的資料
+     * queueName 當下拿到資料的queueName
+     * obj1 為主匹配
+     * match 被匹配
+     */
+    private Set<Order> getMatchData(String queueName) {
+        return redisQueueZSetService.getAllObjectsFromZset(queueName);
+    }
 
-    @Override
-    public void postMatch(RedisQueueZSetService<Order> redisQueueZSetService, String queueName, Order obj1, Order matchedObj) {
+    /*
+    * 進行匹配
+    * */
+    private void matchAndSave(String queueName, Set<Order> orders) {
+        List<Order> matchedData = new ArrayList<>();
+        orders.forEach(obj1 -> {
+            if (!matchedData.contains(obj1)) {
+                List<Order> matched = orders.stream().filter(matchObj -> !matchedData.contains(matchObj) //
+                        && this.isMatch(obj1, matchObj)).toList(); //
+
+                if (!matched.isEmpty()) {
+                    Order matchedObj = matched.get(0);
+                    matchedData.add(obj1);
+                    matchedData.add(matchedObj);
+                    this.postMatch(queueName, obj1, matchedObj);
+                }
+            }
+        });
+    }
+
+    /**
+     * 這裡處理匹配後要做的事，此例為：1.寫入db 2.刪除queue上的資料
+     * queueName 當下拿到資料的queueName
+     * obj1 為主匹配
+     * match 被匹配
+     */
+    public void postMatch(String queueName, Order obj1, Order matchedObj) {
         orderRepository.saveMatchOrder(new OrderMatchEntity(obj1, matchedObj)); //寫入db
         redisQueueZSetService.removeFromZSet(queueName, obj1);
         redisQueueZSetService.removeFromZSet(queueName, matchedObj); // 刪除queue上的資料
     }
-
-
 }
--- src/main/java/com/order/OrderSystem/domain/MatchEngine.java
+++ src/main/java/com/order/OrderSystem/domain/MatchEngine.java
@@ -2,23 +2,24 @@ package com.order.OrderSystem.domain;
 
 import com.order.OrderSystem.application.in.UseCase;
 import com.order.OrderSystem.application.out.RedisLockService;
-import com.order.OrderSystem.application.out.RedisQueueZSetService;
 import com.order.OrderSystem.domain.base.BaseOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.Scheduled;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public abstract class MatchEngine<T extends BaseOrder, U extends UseCase> {
-    @Autowired
-    private ApplicationContext applicationContext;
+
     private Class<U> useCaseClass;
     private U useCase;
+    private Executor executor = Executors.newFixedThreadPool(3);
+    @Autowired
+    private ApplicationContext applicationContext;
+    @Autowired
+    private RedisLockService redisLockService;
 
     public MatchEngine(Class<U> useCaseClass) {
         this.useCaseClass = useCaseClass;
@@ -31,14 +32,17 @@ public abstract class MatchEngine<T extends BaseOrder, U extends UseCase> {
         return this.useCase;
     }
 
-    @Autowired
-    RedisQueueZSetService<T> redisQueueZSetService;
-    @Autowired
-    RedisLockService redisLockService;
-
+    /**
+     * 這裡寫入2個物件如何匹配
+     * obj1 主匹配
+     * obj2 被匹配
+     */
     protected abstract boolean isMatch(T objT, T objU);
 
-    Executor executor = Executors.newFixedThreadPool(3);
+    /**
+     *
+     */
+    protected abstract Runnable lockAndRun(String lockQueueName);
 
     @Scheduled(fixedRate = 1000)
     public void scheduler() {
@@ -47,29 +51,7 @@ public abstract class MatchEngine<T extends BaseOrder, U extends UseCase> {
         queueNames.forEach(queueName -> executor.execute(runMather(queueName)));
     }
 
-    private Runnable runMather(String queueName) {
-        return () -> {
-            Runnable matchRunner = () -> matchAndSave(queueName, redisQueueZSetService.getAllObjectsFromZset(queueName));
-            redisLockService.lockAndDo(queueName, matchRunner);
-        };
+    private Runnable runMather(String lockQueueName) {
+        return () -> redisLockService.lockAndDo(lockQueueName, this.lockAndRun(lockQueueName));
     }
-
-    private void matchAndSave(String queueName, Set<T> orders) {
-        List<T> matchedData = new ArrayList<>();
-        orders.forEach(obj1 -> {
-            if (!matchedData.contains(obj1)) {
-                List<T> matched = orders.stream().filter(matchObj -> !matchedData.contains(matchObj) //
-                        && isMatch(obj1, matchObj)).toList(); //
-
-                if (!matched.isEmpty()) {
-                    T matchedObj = matched.get(0);
-                    this.postMatch(this.redisQueueZSetService, queueName, obj1, matchedObj);
-                    matchedData.add(obj1);
-                    matchedData.add(matchedObj);
-                }
-            }
-        });
-    }
-
-    protected abstract void postMatch(RedisQueueZSetService<T> redisQueueZSetService, String queueName, T obj1, T matchedObj);
 }
